5f21b7d093b1ea9f8c5cbd55fd8df29303d39086
weitao92 <weitao92@vt.edu>
2017-12-25 12:45:06 -0500

    py

---
+++ easy/question12.py
@@ -0,0 +1,37 @@
+dict = {}
+def numOfFactors(n):
+    if n%2 == 0:
+        n /= 2
+    count = 0
+    while n%2 == 0:
+        count += 1
+        n = n / 2
+    count += 1
+
+    divisor = 3
+    while n != 1:
+        temp = 0
+        while n%divisor == 0:
+            temp += 1
+            n = n / divisor
+        count *= (temp+1)
+        divisor += 2
+    return count
+
+previous = 1
+current = 1
+triangle = 0
+i = 3
+while previous * current < 500:
+    if triangle % 2 == 1:
+        dict[triangle]=previous*current
+    previous = current
+    if i in dict:
+        current = dict[i]
+    else:
+        current = numOfFactors(i)
+
+    triangle = (i-1)*i / 2
+    i += 1
+print triangle
+
